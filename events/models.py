50e7d5e54ed34bed072ed749c62ef57d6df2a64e
Felipe <fglf18@gmail.com>
2021-11-08 14:35:43 -0300

    create model for event entity

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+    An event has a category, a name and the payload of data.
+    Also, it contains a timestamp with the data and time it occurred.
+    """
+
+    category = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    data = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
