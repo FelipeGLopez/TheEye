51d0e901622e57959dfe80113b6defaad75d0cbe
Felipe <fglf18@gmail.com>
2021-11-12 22:05:16 -0300

    add fields param to serializer and fix name attribute

--- events/serializers.py
+++ events/serializers.py
@@ -7,12 +7,13 @@ from sessions_app.models import Session
 class EventSerializer(serializers.ModelSerializer):
     session_id = serializers.CharField()
     category = serializers.FloatField()
-    name = serializers.FloatField()
+    name = serializers.CharField()
     data = serializers.JSONField()
     timestamp = serializers.DateTimeField()
 
     class Meta:
         model = Event
+        fields = "__all__"
 
     def create(self, validated_data):
         session_id = validated_data.pop("session_id")
