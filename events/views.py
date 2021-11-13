15d5a4bdce82e57fa9760f9b5d94696844a9b939
Felipe <fglf18@gmail.com>
2021-11-12 22:01:45 -0300

    add inheritance to event viewset

--- events/views.py
+++ events/views.py
@@ -1,11 +1,11 @@
-from rest_framework import mixins
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from events.models import Event
 from events.serializers import EventSerializer
 
 
-class CreateEventViewSet(mixins.CreateModelMixin):
+class CreateEventViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticated]
     queryset = Event.objects.all()
